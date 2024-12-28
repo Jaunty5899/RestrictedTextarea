e9046cfd14ef91fc9c051c7f9ff3882da4f786f1
Jaunty <jaunty5899@gmail.com>
2024-12-28 17:18:07 +0530

    text area created

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div className="container">
+      <textarea name="text" id="txtArea" maxLength={50}></textarea>
+    </div>
+  );
 }
 
 export default App;
