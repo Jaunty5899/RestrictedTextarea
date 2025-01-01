43ee8d1c64f41e89f26a0e484309889d17ec5a79
Jaunty <jaunty5899@gmail.com>
2025-01-01 12:22:10 +0530

    text area refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <div className="container">
-      <textarea name="text" id="txtArea" maxLength={50}></textarea>
+      <textarea
+        className="txtArea"
+        cols={40}
+        rows={4}
+        placeholder="Write anything you want"
+      />
     </div>
   );
 }
